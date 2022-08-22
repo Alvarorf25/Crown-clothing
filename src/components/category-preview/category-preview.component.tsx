d8ccfd02807c92744ff41816a27c711795e1ce53
Alvarorf25 <morales.alvarorf@gmail.com>
2022-08-22 12:01:18 -0400

    converting remaining components to typescript

--- src/components/category-preview/category-preview.component.tsx
+++ src/components/category-preview/category-preview.component.tsx
@@ -1,10 +1,19 @@
+import { FC } from 'react';
+
 import { Link } from 'react-router-dom';
 
 import ProductCard from '../product-card/product-card.component';
 
 import {CategoryPreviewContainer, Title, Preview} from './category-preview.styles';
 
-const CategoryPreview = ({ title, products }) => {
+import { CategoryItem } from '../../store/categories/category.types';
+
+type CategoryPreviewProps = {
+    title: string;
+    products: CategoryItem[];
+};
+
+const CategoryPreview: FC<CategoryPreviewProps> = ({ title, products }) => {
     return (
         <CategoryPreviewContainer>
             <h2>
@@ -12,7 +21,7 @@ const CategoryPreview = ({ title, products }) => {
             </h2>
             <Preview>
                 {
-                    products.filter((_, index) => index < 4).map(product => <ProductCard key={product.id} product={product} />)
+                    products.filter((_, index: number) => index < 4).map(product => <ProductCard key={product.id} product={product} />)
                 }
             </Preview>
         </CategoryPreviewContainer>
--- src/components/product-card/product-card.component.tsx
+++ src/components/product-card/product-card.component.tsx
@@ -1,16 +1,23 @@
+import { FC } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
 import { selectCartItems } from '../../store/cart/cart.selector';
 import { addItemToCart } from '../../store/cart/cart.action';
+import { CategoryItem } from '../../store/categories/category.types';
 
 import {ProductCardContainer, Footer, Name, Price} from './product-card.styles'
 
-const ProductCard = ({ product }) => {
+type ProductCardProps = {
+    product: CategoryItem;
+};
+
+
+const ProductCard: FC<ProductCardProps> = ({ product }) => {
     const { imageUrl, name, price } = product;    
     const dispatch = useDispatch();
-
     const cartItems = useSelector(selectCartItems);
 
     const addProductToCart = () => dispatch(addItemToCart(cartItems, product));
