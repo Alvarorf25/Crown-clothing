0ceda660304fd19a63cd4a986d63bdf09c29cf80
Alvarorf25 <morales.alvarorf@gmail.com>
2022-09-28 12:22:11 -0400

    using hooks useCallback on cart-dropdown and useMemo on cart-item and checkout-item

--- src/components/cart-dropdown/cart-dropdown.component.tsx
+++ src/components/cart-dropdown/cart-dropdown.component.tsx
@@ -1,5 +1,7 @@
-import { useNavigate } from 'react-router-dom';
+import { useCallback } from 'react';
+
 import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import { selectCartItems } from '../../store/cart/cart.selector';
 
@@ -12,7 +14,9 @@ const CartDropdown = () => {
     const cartItems = useSelector(selectCartItems);
     const navigate = useNavigate();    
 
-    const goToCheckoutHandler = () => navigate('/checkout');    
+    const goToCheckoutHandler = useCallback(() => {
+        navigate('/checkout');
+    }, []);    
 
     return (
         <CartDropdownContainer>
--- src/components/checkout-item/checkout-item.component.tsx
+++ src/components/checkout-item/checkout-item.component.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react';
+import { FC, memo } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
@@ -13,7 +13,7 @@ type CheckoutItemProps = {
     cartItem: CartItem;
 };
 
-const CheckoutItem: FC<CheckoutItemProps> = ({cartItem}) => {
+const CheckoutItem: FC<CheckoutItemProps> = memo(({cartItem}) => {
     const { name, imageUrl, price, quantity } = cartItem;
 
     const dispatch = useDispatch();
@@ -40,6 +40,6 @@ const CheckoutItem: FC<CheckoutItemProps> = ({cartItem}) => {
             <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
         </CheckoutItemContainer>
     )
-};
+});
 
 export default CheckoutItem;
